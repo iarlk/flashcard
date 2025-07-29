a4a099e82014fe0a7a61c4f7a94715e0e2a4c632
iarlk <paula.oliveira.eloisa@escola.pr.gov.br>
2025-07-29 16:19:30 -0300

    29/07

---
+++ assets/perguntas.js
@@ -0,0 +1,24 @@
+criarCartao(
+'Buracos Negros',
+'O que são buracos negros?',
+'Buracos negros são regiões do espaço com gravidade tão intensa que nada, nem mesmo a luz, pode escapar.' 
+)
+
+criarCartao(
+    'Universo',
+    'Qual a idade do universo?',
+    'A idade estimada do universo é de 13,8 bilhões de anos. ' 
+    )
+
+    criarCartao(
+        'espaço sideral?' ,
+        'O que é o espaço sideral?' ,
+        'O espaço sideral é o vácuo parcial que existe entre os corpos celestes, preenchido por radiação, partículas e campos magnéticos.' 
+        )
+
+    
+        criarCartao(
+            'universo observável?',
+            'Qual o tamanho do universo observável?',
+          'O universo observável tem cerca de 93 bilhões de anos-luz de diâmetro, ou 46,5 bilhões de anos-luz em qualquer direção a partir da Terra.'
+            )
